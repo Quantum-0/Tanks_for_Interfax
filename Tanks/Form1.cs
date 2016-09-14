2ee8d535bb8d04e0bd024333aec52cb52505701c
Quantum0 <quantum0@mail.ru>
2016-09-14 20:00:57 +0400

    Обработка начального состояния готова

--- Tanks/Form1.cs
+++ Tanks/Form1.cs
@@ -12,9 +12,10 @@ namespace Tanks
 {
     public partial class Form1 : Form
     {
-        int Speed;
         Dictionary<Label, double> Directions = new Dictionary<Label, double>();
         List<Label> Labels = new List<Label>();
+        List<Button> Buttons = new List<Button>();
+        List<Rectangle> Rectangles = new List<Rectangle>();
 
         public Form1()
         {
@@ -24,21 +25,50 @@ namespace Tanks
         private void Form1_Load(object sender, EventArgs e)
         {
             Random rnd = new Random();
-            // Ищем все Labels и заносим в список
+
+            Labels = this.Controls.OfType<Label>().ToList();
+            Buttons = this.Controls.OfType<Button>().ToList();
             
             foreach (var l in Labels)
             {
+                // Меняем свойства label'ов
                 l.Text = "T";
                 l.TextAlign = ContentAlignment.MiddleCenter;
 
-                // Проверяем коллизию с другими элементами
-
+                // Добавляем в список прямоугольников рамку label'a
+                Rectangles.Add(l.Bounds);
 
                 // Создаём направления для всех
                 Directions.Add(l, rnd.NextDouble() * Math.PI * 2);
             }
 
-            timer1.Interval = 1000 / Speed;
+            // Так же записываем рамки всех препятствий
+            foreach (var b in Buttons)
+            {
+                Rectangles.Add(b.Bounds);
+            }
+
+            // Проверяем коллизии
+            for (int i = 0; i < Rectangles.Count; i++)
+            {
+                for (int j = i + 1; j < Rectangles.Count; j++)
+                {
+                    if (Rectangles[i].IntersectsWith(Rectangles[j]))
+                    {
+                        MessageBox.Show("Некорректное исходное состояние");
+                        Close();
+                    }
+                }
+
+                if (i < Labels.Count)
+                    if (Rectangles[i].X < 0 || Rectangles[i].Y < 0 || Rectangles[i].Bottom > this.Height || Rectangles[i].Right > this.Width)
+                    {
+                        MessageBox.Show("Танк пересекает или находится за пределами формы");
+                        Close();
+                    }
+            }
+
+            timer1.Interval = 200;
             timer1.Start();
         }
 
@@ -47,8 +77,7 @@ namespace Tanks
             foreach (var l in Labels)
             {
                 // Перемещаем l
-
-                //l.Top += Math.Cos(Directions[l]);
+                l.Location = new Point(l.Location.X + (int)Math.Cos(Directions[l]), l.Location.Y + (int)Math.Sin(Directions[l]));
 
                 // Проверяем коллизии
                 
@@ -58,8 +87,13 @@ namespace Tanks
 
         private void trackBar1_ValueChanged(object sender, EventArgs e)
         {
-            Speed = trackBar1.Value;
-            timer1.Interval = 1000 / Speed;
+            if (trackBar1.Value != 0)
+            {
+                timer1.Interval = 1000 / trackBar1.Value;
+                timer1.Start();
+            }
+            else
+                timer1.Stop();
         }
     }
 }
